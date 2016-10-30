fa2be5ae6da672ea2b16a5c26744fa1d56355d90
radioactivated <gaowmhs1@gmail.com>
2016-10-30 10:51:02 -0400

    Create initial boilerplate project

---
+++ src/radioactivated/scara/ScaraController.java
@@ -0,0 +1,7 @@
+package radioactivated.scara;
+
+/**
+ * Created by gaow on 10/30/2016.
+ */
+public class ScaraController {
+}
---
+++ src/radioactivated/scara/serial/SerialComm.java
@@ -0,0 +1,8 @@
+package radioactivated.scara.serial;
+// computer running this must have RXTX installed. (http://fizzed.com/oss/rxtx-for-java)
+import gnu.io.*; // added external library; should move to project directory.
+/**
+ * Created by gaow on 10/30/2016.
+ */
+public class SerialComm {
+}
