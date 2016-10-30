a5c0dd1c6f3102bd55b73e73cdbdc9bea59facea
radioactivated <gaowmhs1@gmail.com>
2016-10-30 15:48:28 -0400

    A middling attempt at using jSSC to communicate w/ arduino (need fix)

--- src/radioactivated/scara/ScaraController.java
+++ src/radioactivated/scara/ScaraController.java
@@ -11,9 +11,37 @@ import jssc.SerialPortException;
  */
 public class ScaraController {
     static Scanner inScanner = new Scanner(System.in);
-
+    static final byte readyBytes[] = {1,1,1,1,1,1,1,1};
     public static void main(String[] args) {
         SerialPort port = new SerialPort("COM5");
-        
+        try{
+            port.openPort();
+            port.setParams(9600,SerialPort.BAUDRATE_9600,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+
+            while(port.readBytes(8) != readyBytes) {
+                System.out.println(port.readBytes(8));
+                System.out.println("a");
+                //listen for "ready bytes" from
+                //System.out.println("listening");
+            }
+            System.out.println("Ready");
+        }catch(Exception e) {
+            System.out.println("asdfa");
+            e.printStackTrace();
+        }
+
+        while(true) {
+            try {
+
+                //System.out.println(port.readBytes());
+                System.out.print("Enter a number:");
+                int in = inScanner.nextInt();
+                port.writeInt(in);
+                System.out.println(port.readBytes());
+            } catch (Exception e) {
+
+            }
+        }
+
     }
 }
