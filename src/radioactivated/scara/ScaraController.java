f7d715b852f8cfde3ba85398e93252120f932165
radioactivated <gaowmhs1@gmail.com>
2016-10-30 12:05:56 -0400

    Fill main class with basic code

--- src/radioactivated/scara/ScaraController.java
+++ src/radioactivated/scara/ScaraController.java
@@ -4,4 +4,8 @@ package radioactivated.scara;
  * Created by gaow on 10/30/2016.
  */
 public class ScaraController {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
