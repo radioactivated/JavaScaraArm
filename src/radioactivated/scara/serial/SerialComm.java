77d09f7609e0c87141f7a3edc0d524d442fd11fe
radioactivated <gaowmhs1@gmail.com>
2016-10-30 11:57:27 -0400

    Include tutorial/sample code from Arduino Playground

--- src/radioactivated/scara/serial/SerialComm.java
+++ src/radioactivated/scara/serial/SerialComm.java
@@ -5,4 +5,9 @@ import gnu.io.*; // added external library; should move to project directory.
  * Created by gaow on 10/30/2016.
  */
 public class SerialComm {
+    int baudRate = 9600; // must match on Arduino side
+    String megaPortId = "COM5";
+
+
+
 }
